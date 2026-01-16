ee3629dfc0a795e140499a03a9eeef7bd4ab77f4
Omkar-Kurade <omkarkurade14@gmail.com>
2026-01-16 12:50:04 +0530

    add code in main and crawl

--- crawl.js
+++ crawl.js
@@ -1,13 +1,60 @@
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString)
-    const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
-    if(hostPath.length > 0 && hostPath.slice(-1) === "/"){
-        return hostPath.slice(0 , -1)
-    }
+import { JSDOM } from "jsdom";
 
-    return hostPath
+export async function crawlPage(currentURL){
+    console.log(`actively crawling : ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+        if(resp.status > 399){
+            console.log(`Error in fetch with status code : ${resp.status} on page : ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")){
+             console.log(`non html responce , content type ${contentType},  on page : ${currentURL}`)
+            return
+        }
+        console.log(await resp.text())
+        
+    } catch (error) {
+        console.log(`Error in fetch : ${error.message} , on page ${currentURL}`)
+    }
+   
 }
 
-module.exports = {
-    normalizeURL    
+export function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const anchorElements = dom.window.document.querySelectorAll("a");
+    for (const anchor of anchorElements) {
+        if (anchor.href.slice(0, 1) === "/") {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${anchor.href}`);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log(`Error with relative url ${error.message}`);
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(anchor.href);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log(`Error with absolute url ${error.message}`);
+            }
+        }
+    }
+    return urls;
+}
+
+export function normalizeURL(urlString) {
+    const urlObj = new URL(urlString);
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+    if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+        return hostPath.slice(0, -1);
+    }
+
+    return hostPath;
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,30 +1,89 @@
-const  { normalizeURL } = require("./crawl.js");
-const  {test , expect} = require("@jest/globals");
+import { normalizeURL, getURLsFromHTML }  from "./crawl.js";
+import { test, expect } from "@jest/globals";
 
-test("normalizeURL stripe protocol" , ()=>{
-    const input = 'https://blog.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-})
+test("normalizeURL stripe protocol", () => {
+    const input = "https://blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
 
-test("normalizeURL stripe traling slash" , ()=>{
-    const input = 'https://blog.boot.dev/path/'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-})
+test("normalizeURL stripe traling slash", () => {
+    const input = "https://blog.boot.dev/path/";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
 
-test("normalizeURL capitals" , ()=>{
-    const input = 'https://BLOG.boot.dev/path/'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-})
+test("normalizeURL capitals", () => {
+    const input = "https://BLOG.boot.dev/path/";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
 
-test("normalizeURL stripe http" , ()=>{
-    const input = 'http://blog.boot.dev/path/'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-})
+test("normalizeURL stripe http", () => {
+    const input = "http://blog.boot.dev/path/";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+// test("getURLsFromHTML", () => {
+//     const inputHTMLBody = `
+//     <html>
+//     <body>
+//      <a href="http://blog.boot.dev/path/">Blog Boot dev</a>
+//     </body>
+//     </html>
+//     `;
+//     const inputBaseURL = "http://blog.boot.dev/path/";
+//     const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+//     const expected = ["http://blog.boot.dev/path/"];
+//     expect(actual).toEqual(expected);
+// });
+
+// test("getURLsFromHTML relative", () => {
+//     const inputHTMLBody = `
+//     <html>
+//     <body>
+//      <a href="/path/">Blog Boot dev</a>
+//     </body>
+//     </html>
+//     `;
+//     const inputBaseURL = "/path/";
+//     const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+//     const expected = ["http://blog.boot.dev/path/"];
+//     expect(actual).toEqual(expected);
+// });
+
+// test("getURLsFromHTML both", () => {
+//     const inputHTMLBody = `
+//     <html>
+//     <body>
+//      <a href="/path2/">Blog Boot dev two</a>
+//      <a href="http://blog.boot.dev/path1/">Blog Boot dev one</a>
+//     </body>
+//     </html>
+//     `;
+//     const inputBaseURL = "/path/";
+//     const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+//     const expected = ["http://blog.boot.dev/path2/" , "http://blog.boot.dev/path1/"];
+//     expect(actual).toEqual(expected);
+// });
+
+
+// test("getURLsFromHTML invalide", () => {
+//     const inputHTMLBody = `
+//     <html>
+//     <body>
+//      <a href="invalide">invalide</a>
+//      
+//     </body>
+//     </html>
+//     `;
+//     const inputBaseURL = "/path/";
+//     const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+//     const expected = [];
+//     expect(actual).toEqual(expected);
+// });
